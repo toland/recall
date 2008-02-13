c0bdaad41b15184f6009a391364092fef063a6dc
ptoland <ptoland@thehive.com>
2008-02-13 09:15:17 -0600

    Fleshed out both the client and server with support for commands.

--- client/recall.rb
+++ client/recall.rb
@@ -1,17 +1,43 @@
 module Recall
   
-  class Connection
+  class Client
 
   	def initialize(host="localhost", port=9900)
   		@host, @port = host, port
   	end
 
-    def eval(command)
-      socket = TCPSocket.new(@host, @port)
-  		socket.write(command)
-  		result = socket.read
-  		socket.close
-  		result
+    def connect(who)
+      greeting = read
+      puts greeting
+      response = send_command "CONNECT #{who}"
+      puts response
+    end
+
+    def disconnect
+      @socket.close
+    end
+
+    def echo(what)
+      send_command "ECHO #{what}"
+    end
+
+    def send_command(command)
+  		write command
+  		read
+    end
+
+  protected
+
+    def socket
+      @socket ||= TCPSocket.new(@host, @port)
+    end
+
+    def read
+      socket.readline
+    end
+
+    def write(what)
+      socket.write what
     end
 
   end
--- server/recall.erl
+++ server/recall.erl
@@ -1,11 +1,8 @@
 -module(recall).
--export([start/0, 
-         start/1, 
-         init/1, 
-         loop/1,
-         accept_clients/2, 
-         register_client/2, 
-         client_loop/2]).
+-export([start/0]).
+-export([start_listener/1, accept_clients/2, register_client/2, loop/1, client_loop/2]).
+-compile(export_all).
+
 
 -define(DEFAULT_PORT, 9900).
 
@@ -13,14 +10,13 @@
 -record(client, {socket, name, pid}).
 
 
-start()     -> start(?DEFAULT_PORT).
-start(Port) -> spawn(?MODULE, start, [Port]).
+start() -> 
+    spawn(?MODULE, start_listener, [?DEFAULT_PORT]).
 
-    
-init(Port) ->
+start_listener(Port) ->
     {ok, Socket} = gen_tcp:listen(Port, [{packet, 0}, {active, false}]),
     Accepter = spawn_link(?MODULE, accept_clients, [Socket, self()]),
-	io:fwrite(standard_io, "--> Recall Server Started on port ~B...~n", [Port]),
+	io:fwrite("--> Recall Server Started on port ~B...~n", [Port]),
     loop(#session{socket = Socket, accepter = Accepter, clients = []}).
 
 
@@ -29,7 +25,7 @@ loop(Session = #session{accepter = A, clients = Cs}) ->
         {'new client', Client} ->
             erlang:monitor(process, Client#client.pid),
             Cs1 = [Client|Cs],
-            io:fwrite(user, "--> new connection from ~s\r\n", [Client#client.name]),
+            io:fwrite("--> new connection from ~s\r\n", [Client#client.name]),
             loop(Session#session{clients = Cs1});
 
         {'DOWN', _, process, Pid, _Info} ->
@@ -41,55 +37,69 @@ loop(Session = #session{accepter = A, clients = Cs}) ->
             end;
 
         {'lost client', Client} ->
-            io:fwrite(user, "--> lost connection from ~s\r\n", [Client#client.name]),
+            io:fwrite("--> lost connection from ~s\r\n", [Client#client.name]),
             gen_tcp:close(Client#client.socket),
             loop(Session#session{clients = lists:delete(Client, Cs)});
 
-        {message, Client, Msg} ->
-            io:fwrite(user, "--> <~s> ~s\r\n", [Client#client.name, Msg]),
-            loop(Session);
-
         refresh ->
-            io:fwrite(standard_io, "--> Refreshing Recall server...", []),
+            io:fwrite("--> Refreshing Recall server...", []),
             A ! refresh,
             lists:foreach(fun (#client{pid=CP}) -> CP ! refresh end, Cs),
-            ?MODULE:loop(Session)
+            ?MODULE:loop(Session);
+
+        quit ->
+            io:fwrite("--> Exiting Recall server...", []),
+            A ! quit,
+            lists:foreach(fun (#client{pid=CP}) -> CP ! quit end, Cs)
     end.
 
 
 accept_clients(Socket, Server) ->
-    {ok, Client} = gen_tcp:accept(Socket),
-    spawn(?MODULE, register_client, [Client, Server]),
+    case gen_tcp:accept(Socket) of
+        {ok, Client} -> spawn(?MODULE, register_client, [Client, Server]);
+        {error, Reason} -> io:fwrite("--> Error connecting to client: ~s~n", [Reason]) 
+    end,
     receive
-        refresh -> ?MODULE:accept_clients(Socket, Server)
+        refresh -> ?MODULE:accept_clients(Socket, Server);
+        quit -> gen_tcp:close(Socket)
     after 0 -> accept_clients(Socket, Server)
     end.
 
 register_client(Socket, Server) ->
-    gen_tcp:send(Socket, "Welcome to Recall. Connect with 'CONNECT name'.\r\n"),
+    socket_send(Socket, "Welcome to Recall. Connect with 'CONNECT name'."),
     {ok, N} = gen_tcp:recv(Socket, 0),
     case string:tokens(N, " \r\n") of
         ["CONNECT", Name] ->
             Client = #client{socket=Socket, name=Name, pid=self()},
             Server ! {'new client', Client},
+            socket_send(Socket, "ok"),
             client_loop(Client, Server);
-
+    
         _ ->
             gen_tcp:send(Socket, "That wasn't sensible, sorry."),
             gen_tcp:close(Socket)
     end.
 
-client_loop(Client, Server) ->
-    case gen_tcp:recv(Client#client.socket, 0) of
-        {ok, Recv} ->
-            lists:foreach(fun (S) -> Server ! {message, Client, S} end,
-                          string:tokens(Recv, "\r\n"));
-
-        {error, _} ->
-            Server ! {'lost client', Client}
+client_loop(Client = #client{socket = Socket}, Server) ->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Recv} -> do_command(Socket, split_command(Recv));
+        {error, _} -> Server ! {'lost client', Client}
     end,
     receive
-        refresh -> ?MODULE:client_loop(Client, Server)
+        refresh -> ?MODULE:client_loop(Client, Server);
+        quit -> gen_tcp:close(Socket)
     after 0 -> client_loop(Client, Server)
     end.
 
+socket_send(Socket, Msg) -> gen_tcp:send(Socket, Msg ++ "\r\n").
+
+split_command(CmdStr) ->
+    Cmd = string:sub_word(CmdStr, 1),
+    {Cmd, string:strip(CmdStr -- Cmd)}.
+
+do_command(Socket, {"ECHO", Args}) ->
+    socket_send(Socket, Args),
+    socket_send(Socket, "ok");
+do_command(Socket, {Cmd, _Args}) ->
+    io:fwrite("--> Calling default handler for command ~s...~n", [Cmd]),
+    socket_send(Socket, "ok").
